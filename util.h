fd9f81f6694e5bd1949369819d93d991ebe85b71
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-09-30 00:51:07 +0700

    add guard

--- util.h
+++ util.h
@@ -1,3 +1,6 @@
+#ifndef _UTIL_h
+#define _UTIL_h
+
 #define SIZE_MSG ((1 << 16) - 1)
 #define PORT 8000
 
@@ -8,3 +11,5 @@ struct data {
 
 int get_input_and_send(int fd, struct data *d);
 int receive_data(int fd, struct data *d, const char *type);
+
+#endif
