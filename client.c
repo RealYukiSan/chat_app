b06ef4f13cdee2e52e8d7a1a8c9ce8c0b91145da
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-10-13 21:44:46 +0700

    feat(client): add connect to server functionality

--- client.c
+++ client.c
@@ -1,4 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#include "util.h"
+
+static int connect_server()
+{
+	int fd;
+	struct sockaddr_in addr;
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	inet_pton(AF_INET, server_addr, &addr.sin_addr);
+	addr.sin_port = htons(SERVER_PORT);
+	addr.sin_family = AF_INET;
+	
+	if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("connect");
+		close(fd);
+		return -1;
+	}
+	
+	return fd;
+}
+
 int main(void)
 {
+	int socket_fd;
+	socket_fd = connect_server();
+	if (socket_fd < 0)
+		return -1;
+	
 	return 0;
 }
