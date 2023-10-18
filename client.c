0aec7c31abd96c411755064e780a1e3d689148d9
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-10-18 11:04:59 +0700

    wip(client): add event loop func and client context struct

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+#include <poll.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -5,6 +6,11 @@
 #include <string.h>
 
 #include "util.h"
+
+struct client_ctx {
+	int 		tcp_fd;
+	struct pollfd 	*fds;
+};
 
 static int connect_server()
 {
@@ -30,11 +36,27 @@ static int connect_server()
 	return fd;
 }
 
+static start_event_loop(struct client_ctx *cl_ctx)
+{
+	/**
+	 * TODO:
+	 * 	sebelum mengirim:
+	 * 		- membaca pesan dari input terminal melalui stdin
+	 * 		- mempersiapkan paket yang akan dikirim
+	 * 	mengirim pesan ke server
+	 * 	menerima pesan dari server
+	 * 	setelah menerima:
+	 * 		- mengekstra pesan dengan cara yang benar
+	 * 		- menampilkan pesan yang telah terekstrak
+	*/
+}
+
 int main(void)
 {
-	int socket_fd;
-	socket_fd = connect_server();
-	if (socket_fd < 0)
+	struct client_ctx cl_ctx;
+
+	cl_ctx.tcp_fd = connect_server();
+	if (cl_ctx.tcp_fd < 0)
 		return -1;
 	
 	return 0;
