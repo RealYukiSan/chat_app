7f09bf1bb139902f3fb668aa80aee3bf7efc491d
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-10-18 13:41:11 +0700

    feat(client): handle input event

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <poll.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -9,6 +10,7 @@
 #include "util.h"
 #include "packet.h"
 
+#define PLACE_HOLDER "ME> "
 struct client_ctx {
 	/* tcp sockstream FD */
 	int 		tcp_fd;
@@ -48,7 +50,7 @@ static int connect_server()
 	return fd;
 }
 
-static void start_event_loop(struct client_ctx *cl_ctx)
+static int handle_input(struct client_ctx *cl_ctx)
 {
 	/**
 	 * TODO:
@@ -56,11 +58,55 @@ static void start_event_loop(struct client_ctx *cl_ctx)
 	 * 		- membaca pesan dari input terminal melalui stdin
 	 * 		- mempersiapkan paket yang akan dikirim
 	 * 	mengirim pesan ke server
-	 * 	menerima pesan dari server
-	 * 	setelah menerima:
-	 * 		- transform format/mengekstra pesan dengan cara yang benar
-	 * 		- menampilkan pesan yang telah terekstrak
 	*/
+
+	if (!fgets(cl_ctx->msg, sizeof(cl_ctx->msg), stdin))
+		return -1;
+
+	printf("%s", cl_ctx->msg);
+	cl_ctx->need_reload_prompt = true;
+
+	return 0;
+}
+
+static int handle_events(struct client_ctx *cl_ctx)
+{
+	if (cl_ctx->fds[0].revents & POLLIN) {
+		/**
+		 * TODO:
+		 * 	setelah menerima:
+		 * 		- transform format/mengekstra pesan dengan cara yang benar
+		 * 		- menampilkan pesan yang telah terekstrak
+		*/
+	}
+
+	if (cl_ctx->fds[1].revents & POLLIN) {
+		handle_input(cl_ctx);
+	}
+
+	return 0;	
+}
+
+static void start_event_loop(struct client_ctx *cl_ctx)
+{
+	int nr_ready;
+
+	cl_ctx->need_reload_prompt = true;
+	while (1) {
+		if (cl_ctx->need_reload_prompt) {
+			printf(PLACE_HOLDER);
+			fflush(stdout);
+			cl_ctx->need_reload_prompt = false;
+		}
+		
+		nr_ready = poll(cl_ctx->fds, 2, -1);
+		if (nr_ready < 0) {
+			perror("poll");
+			break;
+		}
+
+		handle_events(cl_ctx);
+	}
 }
 
 static int initialize_ctx(struct client_ctx *cl_ctx)
@@ -72,14 +118,17 @@ static int initialize_ctx(struct client_ctx *cl_ctx)
 	cl_ctx->fds[0].fd = cl_ctx->tcp_fd;
 	cl_ctx->fds[0].events = POLLIN;
 
-	cl_ctx->fds[1].fd = stdin;
+	cl_ctx->fds[1].fd = STDIN_FILENO;
 	cl_ctx->fds[1].events = POLLIN;
+
+	return 0;
 }
 
 int main(void)
 {
 	struct client_ctx cl_ctx;
 	initialize_ctx(&cl_ctx);
+	start_event_loop(&cl_ctx);
 	
 	return 0;
 }
